4ed37c56143030ab833f93be2af6c5dc6ed8002e
James Snyder <james@jrsnyder.tech>
2023-03-29 18:50:18 -0400

    replaced existing SQLAlchemy code w/ Flask SQLAlchemy

--- main.py
+++ main.py
@@ -1,61 +1,56 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import os
-from flask import Flask, render_template
-from flask_mysqldb import MySQL
 from dotenv import load_dotenv
-from connect_planescape_db import *
-from get_charlist import get_charlist
-from get_charinfo import get_charinfo
-from get_deadbooklist import get_deadbooklist
-from get_deadbookcharinfo import get_deadbookcharinfo
 load_dotenv('.env')
 
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:3306/{os.getenv('MYSQL_DB')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
-mysql = connect_planescape_db(app)
 
-# Test list of names
-mylist = ["Garfield", "Odie"]
+class pla(db.Model):
+    charID = db.Column(db.Integer, primary_key=True)
+    playerID = db.Column(db.Integer)
+    charName = db.Column(db.String)
+    charBio = db.Column(db.String)
+    charBlurb = db.Column(db.String)
+    charFaction = db.Column(db.String)
+    charImg = db.Column(db.String)
+    deadbook = db.Column(db.String)
+    epitaph = db.Column(db.String)
+     
+    
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def hello_world():
-    # Grab character names from database
-    charlist = get_charlist(mysql)
-    deadbooklist = get_deadbooklist(mysql)
-    return render_template('index.html', mylist=sorted(charlist), deadbooklist=sorted(deadbooklist))
+
+    return render_template('index.html')
 
 
 @app.route("/character/<string:charName>")
 def char_page(charName):
-    charinfo = get_charinfo(charName=charName, mysql=mysql)
-    deadbooklist = get_deadbooklist(mysql)
-    charlist = get_charlist(mysql)
+    charinfo = db.session.execute(db.select(pla).where(
+    pla.charName.like(f'{charName}%')
+)).scalar()
     print(charinfo)
-    return render_template('character_template.html', mylist=sorted(charlist), charinfo=charinfo, deadbooklist=sorted(deadbooklist))
+    return render_template('character_template.html',charinfo=charinfo)
 
 
 @app.route("/story")
 def story():
-    charlist = get_charlist(mysql)
-    deadbooklist = get_deadbooklist(mysql)
-    return render_template('story.html', mylist=sorted(charlist), deadbooklist=sorted(deadbooklist))
+    return render_template('story.html')
 
-
-@app.route("/deadbook/<string:charName>")
-def deadbook(charName):
-    charlist = get_charlist(mysql)
-    deadbookcharinfo = get_deadbookcharinfo(mysql, charName=charName)
-    deadbooklist = get_deadbooklist(mysql)
-    return render_template('deadbook_template.html', mylist=sorted(charlist), deadbooklist=sorted(deadbooklist), deadbookcharinfo=deadbookcharinfo)
 
 # 404 app handling
-
-
 @app.errorhandler(404)
 def page_not_found(e):
-    charlist = get_charlist(mysql)
-    deadbooklist = get_deadbooklist(mysql)
-    return render_template('404.html', mylist=sorted(charlist), deadbooklist=sorted(deadbooklist)), 404
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
