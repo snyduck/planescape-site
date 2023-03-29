0fd91902199a17000f1faee7ef953467d9f5a680
James Snyder <james@jrsnyder.tech>
2023-03-29 18:51:44 -0400

    deleted old mysqldb functions

--- get_charinfo.py
+++
@@ -1,6 +0,0 @@
-from flask_mysqldb import MySQL
-def get_charinfo(mysql,charName):
-    cur = mysql.connection.cursor()
-    cur.execute(f'SELECT * FROM planescape.charInfo WHERE charName LIKE "{charName}%";')
-    charinfo = cur.fetchall()
-    return charinfo
